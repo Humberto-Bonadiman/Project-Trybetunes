ecc80fbd795cbbf61b177c9871371f401f95473e
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-15 10:43:06 -0400

    Criaçao da classa Header e renderizaçao dos itens

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Album from './pages/Album';
-import Favorites from './pages/Favorites';
-import Login from './pages/Login';
-import Profile from './pages/Profile';
-import ProfileEdit from './pages/ProfileEdit';
-import Search from './pages/Search';
-import NotFound from './pages/NotFound';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <p>TrybeTunes</p>
-        <Route path="/" component={ Login } />
-        <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="*" exact component={ NotFound } />
+        <Header />
       </BrowserRouter>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Profile from '../pages/Profile';
+import ProfileEdit from '../pages/ProfileEdit';
+import Search from '../pages/Search';
+import Album from '../pages/Album';
+import Favorites from '../pages/Favorites';
+import Login from '../pages/Login';
+import NotFound from '../pages/NotFound';
+// import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header data-testid="header-component">
+        <Switch>
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/" exact component={ Login } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </header>
+    );
+  }
+}
+
+/* Header.propTypes = {
+
+}; */
+
+export default Header;
