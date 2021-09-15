30ab58b714314d38b7d859e2ecff26da80cc7dd5
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-15 16:42:10 -0400

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -3,11 +3,43 @@ import Header from '../components/Header';
 // import { BrowserRouter } from 'react-router-dom';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      music: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      music: event.target.value,
+    });
+  }
+
   render() {
+    const { music } = this.state;
+    const lenghtName = 2;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Text Search</p>
+        <form>
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            name="search-artist"
+            onChange={ this.handleChange }
+            value={ music }
+          />
+          <button
+            disabled={ music.length < lenghtName }
+            data-testid="search-artist-button"
+            type="button"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
