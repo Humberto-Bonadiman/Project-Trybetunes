cd5c4bc87ca28c252abcd6381392fc14d96c7048
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-17 12:06:57 -0400

    Parte 1 e parte 2 do requisito 7

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,16 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
-// import { BrowserRouter } from 'react-router-dom';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      musicsArray: [],
+    };
+    this.callGetMusicsFromMusicsApi = this.callGetMusicsFromMusicsApi.bind(this);
+  }
+
+  componentDidMount() {
+    this.callGetMusicsFromMusicsApi();
+  }
+
+  async callGetMusicsFromMusicsApi() {
+    console.log(this.props);
+    const { match: { params: { id } } } = this.props;
+    const musics = await getMusics(id);
+    console.log(musics);
+    this.setState({
+      musicsArray: [...musics],
+      artistName: musics[0].artistName,
+      collectionName: musics[0].collectionName,
+      artworkUrl100: musics[0].artworkUrl100,
+    });
+  }
+
   render() {
+    const { musicsArray, artistName, collectionName, artworkUrl100 } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        <p>Text Album</p>
+        <h1>Album</h1>
+        <article>
+          <img src={ artworkUrl100 } alt={ `Album ${artistName}` } />
+          <h2 data-testid="artist-name">{ artistName }</h2>
+          <h4 data-testid="album-name">{ collectionName }</h4>
+          <MusicCard />
+        </article>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default Album;
