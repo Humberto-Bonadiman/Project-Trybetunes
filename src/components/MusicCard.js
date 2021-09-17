c6a08038b5c04c5b3d941f6cc36b2837f321c47d
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-17 17:48:44 -0400

    Parte 1 requisito 8

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -2,8 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      favorite: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    const { musics: { trackName, previewUrl } } = this.props;
+    const { musics: { trackName, previewUrl, trackId } } = this.props;
+    const { favorite } = this.state;
     return (
       <div>
         <p>{ trackName }</p>
@@ -12,6 +31,15 @@ class MusicCard extends React.Component {
           O seu navegador não suporta o elemento
           <code>audio</code>
         </audio>
+        <label htmlFor="favorite" data-testid={ `checkbox-music-${trackId}` }>
+          Favorita
+          <input
+            type="checkbox"
+            name="favorite"
+            value={ favorite }
+            onChange={ this.handleChange }
+          />
+        </label>
       </div>
     );
   }
@@ -21,6 +49,7 @@ MusicCard.propTypes = {
   musics: PropTypes.shape({
     previewUrl: PropTypes.string.isRequired,
     trackName: PropTypes.string.isRequired,
+    trackId: PropTypes.number.isRequired,
   }).isRequired,
 };
 
