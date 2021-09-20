ba12723828ee31a35526ebfba81c82732131c056
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-19 21:29:41 -0400

    Requisito 11

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
-// import { BrowserRouter } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      name: '',
+      email: '',
+      image: '',
+      description: '',
+    };
+  }
+
+  componentDidMount() {
+    this.callGetUserFromUserApi();
+  }
+
+  async callGetUserFromUserApi() {
+    const user = await getUser();
+    console.log(user);
+    const { name, email, image, description } = user;
+    this.setState({
+      loading: false,
+      name,
+      email,
+      image,
+      description,
+    });
+  }
+
   render() {
+    const { loading, name, email, image, description } = this.state;
+    const loadingTime = <p>Carregando...</p>;
+    const informationProfile = (
+      <article>
+        <p>{ name }</p>
+        <p>{ email }</p>
+        <img src={ image } alt={ `Foto ${name}` } data-testid="profile-image" />
+        <p>{ description }</p>
+        <Link to="/profile/edit">Editar perfil</Link>
+      </article>
+    );
     return (
       <div data-testid="page-profile">
         <Header />
-        <p>Text Profile</p>
+        { loading ? loadingTime : informationProfile }
       </div>
     );
   }
